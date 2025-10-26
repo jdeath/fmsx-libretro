058b3788ccee673f4e2e165b324f69263643e57f
jdeath <>
2025-10-26 09:53:18 -0400

    Updated Code and cheats to support .dsk games

--- cheats/BlueMSXToRetroarchCheat.py
+++ cheats/BlueMSXToRetroarchCheat.py
@@ -9,7 +9,7 @@ def convert_mcf_to_cht(directory):
         if not filename.lower().endswith('.mcf'):
             continue
         
-        print(filename)
+        #print(filename)
         input_path = os.path.join(directory, filename)
         output_path = os.path.join(directory, os.path.splitext(filename)[0] + '.cht')
         
@@ -34,6 +34,12 @@ def convert_mcf_to_cht(directory):
             description = description.strip()
             addr = addr.strip()
             newaddr = int(addr) + int(ramOffset)
+            
+            # if value is negative, it is a .dsk game
+            # must subtract the (negative) offset, not add
+            if newaddr < 0:
+                newaddr = int(addr) - int(ramOffset)
+                
             value = value.strip()
             
             lines.append(f'cheat{cheatNumber}_desc = "{description}"')
